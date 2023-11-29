e2bed37d9918b70d92676b0036769ff124c74753
Jack Weaver <jackdweaver@btinternet.com>
2023-11-29 20:18:16 +0000

    it 2

--- Piece.js
+++ Piece.js
@@ -1,121 +1,151 @@
 // Class to handle and manage box elements called boxHandler
 class boxHandler {
-   // Constructor for initializing boxHandler with position, colour, and stroke
-  constructor(newBox, colour, stroke) {
-      this.boxPos = newBox;
-      this.colour = colour;
-      this.stroke = stroke;
-  }
-  // Method to display the box
-  show() {
-      push();
-      fill(this.colour.r, this.colour.g, this.colour.b);
-      stroke(this.stroke.r, this.stroke.g, this.stroke.b);
-      // Calculate position based on layer and coordinates
-      let y = (-20 - (10 * this.boxPos.layerNum));
-      let x = valueCoordinator(this.boxPos.x);
-      let z = valueCoordinator(this.boxPos.z);
+    // Constructor for initializing boxHandler with position, color and stroke
+    constructor(newBox, colour, stroke) {
+        this.boxPos = newBox
+        this.colour = colour;
+        this.stroke = stroke;
+    }
 
-      console.log("X coordinate for boxPos.x:", x);
-      console.log("Z coordinate for boxPos.z:", z);
+    // Method to display the box
+    show() {
+        push();
+        fill(this.colour.r, this.colour.g, this.colour.b);
+        stroke(this.stroke.r, this.stroke.g, this.stroke.b);
+        // Calculate position based on lyaer and coordinates
+        let y = (-20 - (10 * this.boxPos.layerNum))
+        let x = valueCoordinator(this.boxPos.x)
+        let z = valueCoordinator(this.boxPos.z)
+        translate(x, y, z); // Move to calculated position
+        box(10, 10, 10);
+        pop();
+    }
 
-      translate(x, y, z); // Move to calculated position
-      box(10, 10, 10);
-      pop();
-  }
+    // Method to lower the box by one layer
+    drop() {
+        this.boxPos.layerNum = this.boxPos.layerNum - 1
+    }
 
-  // Method to lower the box by one layer
-  drop() {
-      this.boxPos.layerNum = this.boxPos.layerNum - 1;
-  }
+    // Getter method for the box's position
+    getPos() {
+        return (this.boxPos)
+    }
 
-  // Getter method for the box's position
-  getPos() {
-      return (this.boxPos);
-  }
-
-  // Setter method to update the box's position
-  setPos(newBox) {
-      this.boxPos = newBox;
-  }
+    // Setter method to update the box's position
+    setPos(newBox) {
+        this.boxPos = newBox
+    }
 }
 
 // Function to map grid values to coordinates
 function valueCoordinator(value) {
-  // Returns corresponding coordinate for each grid value
-  if (value == 0) { return (-45) };
-  if (value == 1) { return (-35) };
-  if (value == 2) { return (-25) };
-  if (value == 3) { return (-15) };
-  if (value == 4) { return (-5) };
-  if (value == 5) { return (5) };
-  if (value == 6) { return (15) };
-  if (value == 7) { return (25) };
-  if (value == 8) { return (35) };
-  if (value == 9) { return (45) };
+    // Returns corresponding coordinate for each grid value
+    if (value == 0) { return (-45) }
+    if (value == 1) { return (-35) }
+    if (value == 2) { return (-25) }
+    if (value == 3) { return (-15) }
+    if (value == 4) { return (-5) }
+    if (value == 5) { return (5) }
+    if (value == 6) { return (15) }
+    if (value == 7) { return (25) }
+    if (value == 8) { return (35) }
+    if (value == 9) { return (45) }
 }
 
-// Class representing a game piece composed of multiple boxes
-class piece {
-  // Constructor for creating a new game piece
-  constructor(pieceType, masterLocation, colour, fill) {
-      this.boxArr = [];
-      this.masterLocation = masterLocation;
-      this.pieceType = pieceType;
-      this.colour = colour;
-      this.fill = fill;
-      this.updatePos();
-  }
+// Class representing a game piece composed of multiple boxes 
+class peice {
+    // Constructor for creating a new game piece
+    constructor(peiceType, masterLocation, color, fill) {
+        this.boxArr = []
+        this.masterLocation = masterLocation
+        this.peiceType = peiceType
+        this.color = color;
+        this.fill = fill;
+        this.updatePos()
+    }
 
-  // Method to display the piece
-  show() {
-      // Iterate and display each box in the piece
-      for (let i = 0; i < this.boxArr.length; i++) {
-          this.boxArr[i].show();
-      }
-  }
+    // Method to display the piece
+    show() {
+        // Iterate and display each box in the piece
+        for (let i = 0; i < this.boxArr.length; i++) {
+            this.boxArr[i].show()
+        }
+    }
 
-  
-  // Method to lower each box in the piece by one layer
-  drop() {
-      for (let i = 0; i < this.boxArr.length; i++) {
-          this.boxArr[i].drop();
-      }
-  }
+    // Method to lower each box in the piece by one layer
+    drop() {
+        for (let i = 0; i < this.boxArr.length; i++) {
+            this.boxArr[i].drop()
+        }
+    }
 
-  // Getter for the master position of the piece
-  getMasterPos() {
-      return (this.boxArr[0].getPos());
-  }
+    // Getter for the master position of the piece
+    getMasterPos() {
+        return (this.boxArr[0].getPos())
+    }
+    getAllPos(){
+        let allPos = []
+        for (let i = 0; i < this.boxArr.length; i++) {
+            allPos[i] = this.boxArr[i].getPos()
+        }
+        return(allPos)
+    }
 
-  // Setter to update the master location and reposition the piece
-  setPos(masterLocation) {
-      this.masterLocation = masterLocation;
-      this.updatePos();
-  }
+    // Setter to update the master location reposition the piece
+    setPos(masterLocation) {
+        this.masterLocation = masterLocation
+        this.updatePos()
+    }
 
-  // Method to update the position of each box in the piece
-  updatePos() {
-      if (this.pieceType == 0) {
-          //2x2box
-          // [Creates 8 boxHandler instances for each box in the piece]
-          this.boxArr[0] = new boxHandler({ x: this.masterLocation.x, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z }, this.colour, this.fill);
-          this.boxArr[1] = new boxHandler({ x: this.masterLocation.x + 1, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z }, this.colour, this.fill);
-          this.boxArr[2] = new boxHandler({ x: this.masterLocation.x, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z + 1 }, this.colour, this.fill);
-          this.boxArr[3] = new boxHandler({ x: this.masterLocation.x + 1, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z + 1 }, this.colour, this.fill);
-          this.boxArr[4] = new boxHandler({ x: this.masterLocation.x, layerNum: this.masterLocation.layerNum + 1, z: this.masterLocation.z }, this.colour, this.fill);
-          this.boxArr[5] = new boxHandler({ x: this.masterLocation.x + 1, layerNum: this.masterLocation.layerNum + 1, z: this.masterLocation.z }, this.colour, this.fill);
-          this.boxArr[6] = new boxHandler({ x: this.masterLocation.x, layerNum: this.masterLocation.layerNum + 1, z: this.masterLocation.z + 1 }, this.colour, this.fill);
-          this.boxArr[7] = new boxHandler({ x: this.masterLocation.x + 1, layerNum: this.masterLocation.layerNum + 1, z: this.masterLocation.z + 1 }, this.colour, this.fill);
-      }
+    updatePos() {
+        if (this.peiceType == 0) {
+            //2x2x2
+            // [Creates 9 boxHandler instances for each box in the piece]
+            this.boxArr[0] = new boxHandler({ x: this.masterLocation.x, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[1] = new boxHandler({ x: this.masterLocation.x + 1, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[2] = new boxHandler({ x: this.masterLocation.x, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z + 1 }, this.color, this.fill);
+            this.boxArr[3] = new boxHandler({ x: this.masterLocation.x + 1, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z + 1 }, this.color, this.fill);
+            this.boxArr[4] = new boxHandler({ x: this.masterLocation.x, layerNum: this.masterLocation.layerNum + 1, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[5] = new boxHandler({ x: this.masterLocation.x + 1, layerNum: this.masterLocation.layerNum + 1, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[6] = new boxHandler({ x: this.masterLocation.x, layerNum: this.masterLocation.layerNum + 1, z: this.masterLocation.z + 1 }, this.color, this.fill);
+            this.boxArr[7] = new boxHandler({ x: this.masterLocation.x + 1, layerNum: this.masterLocation.layerNum + 1, z: this.masterLocation.z + 1 }, this.color, this.fill);
+        }
+        if (this.peiceType == 1) {
+            //tPeice
+            this.boxArr[0] = new boxHandler({ x: this.masterLocation.x, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[1] = new boxHandler({ x: this.masterLocation.x + 1, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[2] = new boxHandler({ x: this.masterLocation.x + 2, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[3] = new boxHandler({ x: this.masterLocation.x + 1, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z + 1 }, this.color, this.fill);
+        }
+        if (this.peiceType == 2) {
+            //lPeice
+            this.boxArr[0] = new boxHandler({ x: this.masterLocation.x, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[1] = new boxHandler({ x: this.masterLocation.x + 1, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[2] = new boxHandler({ x: this.masterLocation.x + 2, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[3] = new boxHandler({ x: this.masterLocation.x, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z + 1 }, this.color, this.fill);
+        }
+        if (this.peiceType == 3) {
+            //line
+            this.boxArr[0] = new boxHandler({ x: this.masterLocation.x, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[1] = new boxHandler({ x: this.masterLocation.x + 1, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[2] = new boxHandler({ x: this.masterLocation.x + 2, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z }, this.color, this.fill);
+            this.boxArr[3] = new boxHandler({ x: this.masterLocation.x + 3, layerNum: this.masterLocation.layerNum, z: this.masterLocation.z }, this.color, this.fill);
+        }
 
-  }
+    }
 
-  // Method to get the dimensions of the piece
-  getDimensions() {
-      if (this.pieceType == 0) {
-        // Dimensions for a 2x2x2 piece
-        return ({ x: 2, y: 2, z: 2 });
-      }
-  }
+    getDimensions() {
+        if (this.peiceType == 0) {
+            return ({ x: 2, y: 2, z: 2 })
+        }
+        if (this.peiceType == 1) {
+            return ({ x: 3, y: 1, z: 2 })
+        }
+        if (this.peiceType == 2) {
+            return ({ x: 3, y: 1, z: 2 })
+        }
+        if (this.peiceType == 3) {
+            return ({ x: 4, y: 1, z: 1 })
+        }
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 // Variables
-let myTetrisgridL;
-let myTetrisgridR;
-let myTetrisgridF;
-let timer = 500;
+let mytetrisGridL;
+let mytetrisGridR;
+let mytetrisGridF;
+let timer = 100;
+let peiceQueue
+let queuePointer = 0
+let gameProgression = "play"
 
 // The setup function is called once at the beginning to initialize the canvas and settings.
 function setup() {
@@ -10,36 +13,109 @@ function setup() {
   rectMode(CENTER); // Configure rectangle drawing mode to be centered
   angleMode(DEGREES); // Utilize degrees for angle measurements
 
-   // Create new camera objectS and set its initial position and target
+  // Create new camera objectS and set its initial position and target
   currentcamera = new devCam(-250, -300, 250, 0, -100, 0);
 
   // Invokes the Gravity function at regular intervals defined by the timer variable which is set to 500
   setInterval(() => Gravity(), timer);
 
-
   // Create the left Tetris grid object with specified properties
-  myTetrisgridL = new Tetrisgrid(100, 300, 0, 50, -150, 1, 0, 90, 255, { r: 0, g: 0, b: 0 });
+  mytetrisGridL = new tetrisGrid(100, 300, 0, 50, -155, 1, 0, 90, 255, { r: 0, g: 0, b: 0 });
 
   // Create the right Tetris grid object with specified properties
-  myTetrisgridR = new Tetrisgrid(100, 300, 0, 0, -150, -50, 0, 0, 255, { r: 0, g: 0, b: 0 });
+  mytetrisGridR = new tetrisGrid(100, 300, 0, 0, -155, -50, 0, 0, 255, { r: 0, g: 0, b: 0 });
 
   // Create the floor of the tetris grid with specified properties
-  myTetrisgridF = new Tetrisgrid(100, 100, 0, 0, 0, 0, 90, 0, 255, { r: 0, g: 0, b: 0 });
-  
-  // Create the piece with specified properties
-  activeBox = new piece(0, { x: 0, layerNum: 6, z: 0, }, { r: 0, g: 0, b: 255 }, { r: 255, g: 255, b: 255 })
+  mytetrisGridF = new tetrisGrid(100, 100, 0, 0, -5, 0, 90, 0, 255, { r: 0, g: 0, b: 0 });
+
+
+  box1 = new peice(0, { x: 0, layerNum: 30, z: 0, }, { r: 0, g: 0, b: 255 }, { r: 255, g: 255, b: 255 })
+  peiceQueue = [box1]
 }
 
-// Creates a gravity function
+
+// Creates a gravity function that calls piece
 let Gravity = () => {
-  if (!(activeBox.getMasterPos().layerNum < 0)) {
-    activeBox.drop()
+  peiceDropLogic()
+}
+
+// Function to manage the logic for dropping a game piece
+function peiceDropLogic() {
+  // Check if the active piece is above the bottom layer, there's no collision, and the game is in 'play' mode
+  if ((!(peiceQueue[queuePointer].getMasterPos().layerNum < 0)) && !(collisionDetection()) && gameProgression == "play") {
+    // If all conditions are met, drop the piece down by one layer
+    peiceQueue[queuePointer].drop();
+  } else {
+    // If the piece cannot be dropped (either it's at the bottom, there's a collision, or the game is not in 'play' mode)
+
+    // Move to the next piece in the queue
+    queuePointer++;
+
+    // Generate a new piece with random type and color
+    peiceQueue[queuePointer] = new peice(
+      Math.round(Math.random() * 3), // Randomly select the piece type
+      { x: 0, layerNum: 30, z: 0 }, // Start at a high layer
+      // Generate random colors for the piece
+      { r: Math.round(Math.random() * 255), g: Math.round(Math.random() * 255), b: Math.round(Math.random() * 255) },
+      { r: Math.round(Math.random() * 255), g: Math.round(Math.random() * 255), b: Math.round(Math.random() * 255) }
+    );
+
+    let xRandom;
+    let zRandom;
+
+    // Generate a random x position, ensuring it's within the grid boundaries
+    do {
+      xRandom = Math.round(Math.random() * 8);
+    } while (xRandom >= (9 - peiceQueue[queuePointer].getDimensions.x));
+
+    // Generate a random z position, also within grid boundaries
+    do {
+      zRandom = Math.round(Math.random() * 8);
+    } while (zRandom >= (9 - peiceQueue[queuePointer].getDimensions.z));
+
+    // Set the position of the new piece based on the random x and z coordinates
+    peiceQueue[queuePointer].setPos({ x: xRandom, layerNum: 30, z: zRandom });
   }
 }
 
-// Function to access the current camera object
-function cam() {
-  return currentcamera;
+// Function to detect if there's a collision between the currently moving piece and any other piece in the queue
+function collisionDetection() {
+  // Retrieve the positions of all boxes in the currently moving piece
+  let peiceMoveBoxPos = peiceQueue[queuePointer].getAllPos();
+
+  // Iterate through the entire piece queue, excluding the currently moving piece
+  for (let i = 0; i < (peiceQueue.length - 1); i++) {
+    // Retrieve the positions of all boxes in the current piece in the queue
+    let peiceBoxPos = peiceQueue[i].getAllPos();
+
+    // Iterate through each box in the current piece in the queue
+    for (let j = 0; j < peiceBoxPos.length; j++) {
+      
+      // Iterate through each box in the currently moving piece
+      for (let k = 0; k < peiceMoveBoxPos.length; k++) {
+        
+        // Check if any box in the moving piece is directly above any box in the current piece
+        if (peiceBoxPos[j].layerNum == (peiceMoveBoxPos[k].layerNum - 1)){
+          
+          // Check if the boxes are aligned in both x and z coordinates (meaning a collision would occur)
+          if (peiceBoxPos[j].x == peiceMoveBoxPos[k].x && peiceBoxPos[j].z == peiceMoveBoxPos[k].z) {
+            // Collision detected
+
+            // If the collision occurs at a high layer (>= 25), change the game progression to 'over'
+            if (peiceMoveBoxPos[k].layerNum >= 25){
+              gameProgression = "over";
+            }
+
+            // Return true, indicating a collision has occurred
+            return true;
+          }
+        }
+      }
+    }
+  }
+
+  // Return false if no collision is detected
+  return false;
 }
 
 function draw() {
@@ -50,57 +126,71 @@ function draw() {
 
 // The inputs function handles Inputs from the user
 function inputs() {
-  // Empty for now
+  if(keyIsDown(32)){
+    peiceDropLogic()
+  }
 }
 
 // The processes function handles data processing and calculations.
 function processes() {
-  // Empty for now
+  if(gameProgression == "over"){
+    console.log("Game Over")
+  }
 }
 
 // The outputs function handles displaying visual outputs on the canvas.
 function outputs() {
   background(255, 255, 255);
-  currentcamera.show(); // Displays the camera 
+  currentcamera.show(); // Displays the camera
   stroke(255);
-  activeBox.show()// Displays the box
-  //myLPiece.show();
+  for (let i = 0; i < peiceQueue.length; i++) {
+    peiceQueue[i].show()
+  }
   stroke('lime')
-  myTetrisgridL.show(); // Display the left Tetris grid element
-  myTetrisgridR.show(); // Display the right Tetris grid element
-  myTetrisgridF.show(); // Display the front Tetris grid element
+  mytetrisGridL.show(); // Display the left Tetris grid element
+  mytetrisGridR.show(); // Display the right Tetris grid element
+  mytetrisGridF.show(); // Display the front Tetris grid element
 }
 
+
+
 function keyPressed() {
-  if (keyCode == '68') {
-    //w
-    let oldPos = activeBox.getMasterPos()
-    if (!(oldPos.x >= (9 - (activeBox.getDimensions().x - 1)))) {
-      activeBox.setPos({ x: oldPos.x + 1, layerNum: oldPos.layerNum, z: oldPos.z })
-    }
-  }
-
-  if (keyCode == '65') {
-    //s
-    let oldPos = activeBox.getMasterPos()
-    if (!(oldPos.x <= 0)) {
-      activeBox.setPos({ x: oldPos.x - 1, layerNum: oldPos.layerNum, z: oldPos.z })
-    }
-  }
-
+  // replace box1 with activepeice
   if (keyCode == '87') {
-    //a
-    let oldPos = activeBox.getMasterPos()
-    if (!(oldPos.z <= 0)){
-      activeBox.setPos({ x: oldPos.x, layerNum: oldPos.layerNum, z: oldPos.z - 1 })
+    //w
+    let oldPos = peiceQueue[queuePointer].getMasterPos()
+    if (!(oldPos.x >= (9 - (peiceQueue[queuePointer].getDimensions().x - 1)))) {
+      //VALIDATION, REMOVE IF AND ADD INTO WRITEUP FOR MARKS
+      peiceQueue[queuePointer].setPos({ x: oldPos.x + 1, layerNum: oldPos.layerNum, z: oldPos.z })
     }
   }
 
   if (keyCode == '83') {
     //s
-    let oldPos = activeBox.getMasterPos()
-    if (!(oldPos.z >= (9 - (activeBox.getDimensions().z - 1)))) {
-      activeBox.setPos({ x: oldPos.x, layerNum: oldPos.layerNum, z: oldPos.z + 1 })
+    let oldPos = peiceQueue[queuePointer].getMasterPos()
+    if (!(oldPos.x <= 0)) {
+      //VALIDATION, REMOVE IF AND ADD INTO WRITEUP FOR MARKS
+      peiceQueue[queuePointer].setPos({ x: oldPos.x - 1, layerNum: oldPos.layerNum, z: oldPos.z })
     }
   }
+
+  if (keyCode == '65') {
+    //a
+    let oldPos = peiceQueue[queuePointer].getMasterPos()
+    if (!(oldPos.z <= 0)) {
+      //VALIDATION, REMOVE IF AND ADD INTO WRITEUP FOR MARKS
+      peiceQueue[queuePointer].setPos({ x: oldPos.x, layerNum: oldPos.layerNum, z: oldPos.z - 1 })
+    }
+  }
+
+  if (keyCode == '68') {
+    //d
+    let oldPos = peiceQueue[queuePointer].getMasterPos()
+    if (!(oldPos.z >= (9 - (peiceQueue[queuePointer].getDimensions().z - 1)))) {
+      //VALIDATION, REMOVE IF AND ADD INTO WRITEUP FOR MARKS
+      peiceQueue[queuePointer].setPos({ x: oldPos.x, layerNum: oldPos.layerNum, z: oldPos.z + 1 })
+    }
+  }
+  //console.log(event)
 }
+// how to break code: ands and ors, i j k, -1, ==
